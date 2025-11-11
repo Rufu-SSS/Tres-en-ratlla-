f53afc5d5626c1d93863a4c235e125208ed1e973
Rufu-SSS <polplanashoms@girona.lasalle.cat>
2025-11-11 13:18:59 +0100

    Codi acabat i comentat

--- Tres en ratlla/Tres en ratlla/Tres en ratlla.cpp
+++ Tres en ratlla/Tres en ratlla/Tres en ratlla.cpp
@@ -1,4 +1,18 @@
-﻿#include <iostream>
+﻿
+
+
+//=====================================================================//
+// LLIBRERIES I NAMESPACES - continguts
+//=====================================================================//
+// -> Iostream: per fer servir els cout i cin
+// -> Vector: per emmagatzemar info dins "caixes"
+// -> Stdexcept: per estalviar-nos els errors
+// -> Header Alumne1.h: funcions bàsiques pel funcionament del programa
+// -> Header Alumne2.h: funcions bàsiques pel funcionament del programa
+// -> Algorithm: per a fer servir funcions matemàtiques
+// -> Using namespace std: per estalviar-nos d'escriure "std::" 
+//=====================================================================//
+#include <iostream>
 #include <vector>
 #include <stdexcept>
 #include "Alumne1.h"
@@ -6,7 +20,49 @@
 #include <algorithm>
 using namespace std;
 
+
 class Joc {
+//=====================================================================//
+// CLASSE JOC - continguts
+//=====================================================================//
+// Privat:
+//     - Vector tauler: emmagatzema la info que posem al tauler
+//     - Alumne1*: creem un punter fent referència a alumne1
+//     - Alumne2*: creem un altre punter fent referència a alumne2
+//     - Char tornActual: creem una variable que digui de qui es el torn
+// Públic:
+//     - Funció Joc(...): constructor que lliga els punters amb variables,
+//     inicia el tauler i assigna el torn inicial/actual al jugador 1 amb 
+//     una altra funció.
+//     - Funció inicialitzarTauler(): al principi de cada partida, tant 
+//     sigui la primera com la pròxima, inicialitzarem el tauler a les 
+//     mides desitjades i afegirem espais en blanc per a posicions en 
+//     concret.
+//     - Funció imprimirTauler(): tal com diu la funció, imprimeix el 
+//       tauler del joc amb els espais en blanc creats a partir de la
+//     funció anterior (tauler=vector<...>(9, ' ');).
+//     - Funció esMovimentVàlid(...): comprova si es pot jugar una fitxa
+//     en el teu torn en una posició en concret, en cas que no es pugui 
+//     que avisi.
+//     - Funció ferMoviment(...): juga el teu torn la posició (si no està
+//     ja ocupada) i crida la funció anterior per revisar si s'hi pot 
+//     jugar o no.
+//     - Funció comprovarGuanyador(): Segons el torn que sigui i 
+//     pràcticament totes les combinacions de victòria, enviem el missatge
+//     de victòria pel jugador que ha guanyat.
+//     - Funció taulaPlena(): quan no es puguin jugar més 'fitxes' a les 
+//     posicions dels taulers, avisa i canvia l'estat del booleà a true.
+//     - Funció canviarTorn(): tal com diu, canvia el torn del jugador 1
+//     al jugador 2, res més que això.
+//     - Funció reiniciarPartida(): quan els jugadors vulguin fer una 
+//     altra partida després de l'anterior, re-inicialitzarem el tauler 
+//     com estava al principi (buit) i assignarem el torn actual al jug1.
+//     - Funció jugar(): fes tot el bucle del joc, imprimeix el tauler i 
+//     crea un bucle on mentre la condició sigui certa/true funcioni tot:
+//     assigna el torn actual als jugadors amb les funcions de les classes.
+//     També mira qui guanya o empata i ho diu per consola, al final 
+//     pregunta si es vol reiniciar la partida i fer-ne una altra.
+//=====================================================================//
 private:
     vector<char> tauler;
     Alumne1* alumne1;
@@ -88,8 +144,26 @@ public:
     } 
 };
 
-// ---------- MAIN ----------
+
 int main() {
+//=====================================================================//
+// FUNCIÓ PRINCIPAL - continguts
+//=====================================================================//
+// Variables inicials: 
+//     - Bool jugant
+//     - String resposta, nom1, nom2
+//     - Alumne1 a1(nom1); Alumne2 a2(nom2);
+//     - Joc joc(&a1, &a2);
+// Bucle de joc:
+//     - While(jugant==true): mentre es compleixi la condició que fa true
+//     el bucle, repetirà infinites vegades el contingut dins seu. Crida 
+//     la funció jugar() de la classe joc que permet a l'usuari 
+//     interactuar amb el terminal i en finalitzar pregunta si vol 
+//     continuar jugant o no, en cas de posar malament l'input avisarà a 
+//     l'usuari, tot seguit donarà els resultats finals de victòries i 
+//     compararà els stats dels dos jugadors per veure qui ha guanyat més 
+//     vegades.
+//=====================================================================//
     bool jugant=true;
     string resposta;
     string nom1, nom2; cout << "Nom Alumne1 (X): "; cin >> nom1;
